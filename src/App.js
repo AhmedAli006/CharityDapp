b6c27265ca2dcd679e20022be9e10584e3980984
AhmedAli006 <ahmed.spidy444@gmail.com>
2023-01-07 16:50:31 +0500

    upload to goerli network

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import { ethers } from "ethers";
 import contractABI from "./abi/giveForeverAbi.json";
 import { useState } from "react";
 
-const contractAdress = "0xC8d0278966A8979B7c3e5c28EDe60cb023b67071";
+const contractAddress = "0xB7a61314912151d8b6d3396eD0Ad0E27C899ab2d";
 let provider = new ethers.providers.Web3Provider(window.ethereum);
-let contract = new ethers.Contract(contractAdress, contractABI, provider);
+let contract = new ethers.Contract(contractAddress, contractABI, provider);
 let signer;
 
 function App() {
-const connect = async () => {
-  await provider.send("eth_requestAccounts", []);
-  signer = provider.getSigner();
-  contract = new ethers.Contract(contractAdress, contractABI, signer);
 
-  alert(greeting);
-  updateBalances();
+  const [donated, setDonated] = useState(0);
+  const [lidoBalance, setLidoBalance] = useState(0);
+  const [surplus, setSurplus] = useState(0);
+    const [connectionSatus,setConnection] = useState('Not Connected');
+
+
+const connect = async () => {
+await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+    contract = new ethers.Contract(contractAddress, contractABI, signer);
+    const userAddress = await signer.getAddress();
+    const networkData = await provider.getNetwork();
+    let networkName = 'unknown';
+    if (networkData.chainId === 1) networkName = 'mainnet';
+    if (networkData.chainId === 5) networkName = 'goerli';
+
+    console.log(userAddress);
+    setConnection(`Connected to ${networkName} ${userAddress}`);
+    updateBalances();
 };
 
 const deposit = async () => {
@@ -43,10 +56,6 @@ const updateBalances = async () => {
 setTimeout(() => {
   updateBalances();
 }, 1000);
-
-  const [donated, setDonated] = useState(0);
-  const [lidoBalance, setLidoBalance] = useState(0);
-  const [surplus, setSurplus] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -78,11 +87,11 @@ setTimeout(() => {
           <div className="App-contract">
             Contract{" "}
             <a
-              href="https://etherscan.io/address/0xf0a8B14F0A9Ae12c994872028d3B520D303A8F68"
+              href="https://etherscan.io/address/0xd9145CCE52D386f254917e481eB44e9943F39138"
               target="_blank"
               el="noreferrer"
             >
-              0xf0a8B14F0A9Ae12c994872028d3B520D303A8F68
+              0xd9145CCE52D386f254917e481eB44e9943F39138
             </a>
           </div>
         </div>
